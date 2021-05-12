35334e3ac9790307ffe86479fdcfce2b44209c6e
Kevin Sarges <kevinsarges08@gmail.com>
2021-05-12 11:06:27 -0300

    Dotenv Config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
-require('dotenv').config();
 
 const routesMunicipios = require('./routes/routesMunicipios');
 const routesPraias = require('./routes/routesPraias')
