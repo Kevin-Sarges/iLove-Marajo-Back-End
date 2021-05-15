755046f1f3d0919c5708e67b103b85971ead9db7
Kevin Sarges <kevinsarges08@gmail.com>
2021-05-14 21:21:19 -0300

    Alterando

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,7 @@ module.exports = {
     connection: process.env.DATABASE_URL,
 
     migrations: {
-      tableName: `${__dirname}/src/database/migrations`
+      tableName: './src/database/migrations'
     }
   }
 };
