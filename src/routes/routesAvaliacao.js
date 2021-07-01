4efcd43d8df61bb088df90e8a461fb754f8b4363
Kevin Sarges <kevinsarges08@gmail.com>
2021-07-01 15:27:52 -0300

    Add a rota para media

--- src/routes/routesAvaliacao.js
+++ src/routes/routesAvaliacao.js
@@ -2,6 +2,7 @@ const routes = require('express').Router();
 const avaliacao = require('../controllers/controllerAvaliacao');
 
 routes.get('/avaliacoes-praia', avaliacao.listaAvaliacoes);
+routes.get('/media-avaliacoes', avaliacao.mediaAvaliacao);
 
 routes.post('/avaliar-praia', avaliacao.avaliar);
 
