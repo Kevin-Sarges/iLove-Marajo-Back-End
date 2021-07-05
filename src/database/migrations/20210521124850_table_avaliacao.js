ff6bc5b0dd1525a61f255d96ae2fca5be65501b8
Kevin Sarges <kevinsarges08@gmail.com>
2021-07-05 18:33:51 -0300

    Ajustando tabela de avaiaçãoo

--- src/database/migrations/20210521124850_table_avaliacao.js
+++ src/database/migrations/20210521124850_table_avaliacao.js
@@ -1,13 +1,13 @@
 
 exports.up = function(knex) {
   return knex.schema.createTable('avaliacao', table => {
-    table.increments('id_avaliacao').primary();
+    table.increments('id_avaliacao').notNullable().primary();
     table.text('nome_usuario');
     table.float('nota');
     table.text('comentario');
 
     table.integer('id_local');
-    table.foreign('id_local').references('praia.id_praia');
+    table.foreign('id_local').references('praias.id_praia');
   });
 };
 
