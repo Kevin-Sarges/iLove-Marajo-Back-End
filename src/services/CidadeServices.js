ec5a36aa9fd843f8a21535840e579f2212a5c4c1
Kevin-Sarges <kevinsarges08@gmail.com>
2021-09-24 18:52:26 -0300

    Add camada de services

---
+++ src/services/CidadeServices.js
@@ -0,0 +1,30 @@
+import { Op } from 'sequelize';
+import { Cidade } from '../models/Cidade';
+
+class CidadeServices {
+  async index() {
+    const cidade = await Cidade.findAll();
+
+    return cidade;
+  };
+
+  async show({ nome_cidade }) {
+    const cidade = await Cidade.findAll({
+      where: {
+        nome_cidade: {
+          [Op.iLike]: `${nome_cidade}%`
+        }
+      }
+    });
+
+    return cidade;
+  };
+
+  async store({ nome_cidade }) {
+    const criarCidade = await Cidade.create({ nome_cidade });
+
+    return criarCidade;
+  };
+}
+
+export { CidadeServices };
---
+++ src/services/LocalServices.js
@@ -0,0 +1,79 @@
+import { Op } from 'sequelize';
+import { Cidade } from '../models/Cidade';
+import { Local } from '../models/Local';
+
+class LocalServices {
+  async index({ id_cidade }) {
+    const cidade = await Cidade.findByPk(id_cidade, {
+      include: {
+        association: 'local',
+      }
+    });
+
+    if(cidade === null) {
+      return res.status(404).json({ Error: 'Cidade não encontrado' });
+    }
+
+    return cidade;
+  };
+
+  async locais({ id_cidade, local }) {
+    const buscaLocal = await Cidade.findAll({
+      where: { id: id_cidade },
+
+      include: [
+        {
+          association: 'local',
+          where: {
+            local: {
+              [Op.iLike]: `${ local }%`
+            }
+          }
+        }
+      ]
+    });
+
+    return buscaLocal;
+  };
+
+  async show({ id_cidade, nome_local }) {
+    const buscandoNomeLocal = await Cidade.findAll({
+      where: { id: id_cidade },
+
+      include: [
+        {
+          association: 'local',
+          where: {
+            nome_local: {
+              [Op.iLike]: `${ nome_local }%`
+            }
+          }
+        }
+      ]
+    });
+
+    return buscandoNomeLocal;
+  };
+
+  async store({ local, nome_local, foto, lat, lon, descricao, id_cidade }) {
+    const cidade = await Cidade.findByPk(id_cidade);
+
+    if(!cidade) {
+      return res.status(400).json({ error: 'Cidade não encontrada' });
+    }
+
+    const criarLocal = await Local.create({
+      local,
+      nome_local,
+      foto,
+      lat,
+      lon,
+      descricao,
+      id_cidade
+    });
+
+    return criarLocal;
+  };
+}
+
+export { LocalServices };
